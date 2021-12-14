73659ebb21c4361cd25ab41d35029667d425fb15
vuk4asin <vukasin.tonic@gmail.com>
2021-12-14 05:49:19 -0800

    doradjivano

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { LoginComponent } from './login/login.component';
+import { ProductFormComponent } from './admin/product-form/product-form.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { LoginComponent } from './login/login.component';
     OrderSuccessComponent,
     MyOrdersComponent,
     AdminProductsComponent,
-    AdminOrdersComponent
+    AdminOrdersComponent,
+    ProductFormComponent
   ],
   imports: [
     BrowserModule,
@@ -42,7 +44,7 @@ import { LoginComponent } from './login/login.component';
       {path:'login', component:LoginComponent },
       {path:'admin/products', component: AdminProductsComponent},
       {path:'admin/orders', component: AdminOrdersComponent},
-
+      {path:'admin/products/new', component:ProductFormComponent}
     ])
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
 
-  constructor() { }
+  constructor() { 
+    
+      }
+
+  
 
   ngOnInit(): void {
   }
 
+
+
+    
 }
